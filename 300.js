2044dfe2aa7a96d92f7c231bcfa0b19636333673
dirayag <95455839+dirayag@users.noreply.github.com>
2021-12-16 22:16:35 +0700

    Create 300.js

---
+++ 300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '032e2d77da478cc447a1aa38e5be0ec8',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://inflictgive.com/032e2d77da478cc447a1aa38e5be0ec8/invoke.js"></scr' + 'ipt>');
