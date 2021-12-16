703054f622f6d147cdf7ae13c91e227ffd06a4c8
dirayag <95455839+dirayag@users.noreply.github.com>
2021-12-16 22:15:17 +0700

    Create 728.js

---
+++ 728.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'b5e240a04cdb715398965aa37ed6d31d',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://inflictgive.com/b5e240a04cdb715398965aa37ed6d31d/invoke.js"></scr' + 'ipt>');
