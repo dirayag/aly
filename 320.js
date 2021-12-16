58c95370c88e195b294d14fc64ca09a04b7a5e50
dirayag <95455839+dirayag@users.noreply.github.com>
2021-12-16 22:17:17 +0700

    Create 320.js

---
+++ 320.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '8a72538b2e448c93a1a503886d2cfd7e',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://inflictgive.com/8a72538b2e448c93a1a503886d2cfd7e/invoke.js"></scr' + 'ipt>');
